edc560f16f508d9f3e875351e86097cc7c3d3b57
Нуфтанчики <300225@mail.ru>
2020-10-31 23:46:21 +0300

    Создал канвас

---
+++ main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from tkinter import messagebox
+import time
+
+tk = Tk()
+app_running = True
+
+size_canvas_x = 800
+size_canvas_y = 800
+
+def on_closing():
+    global app_running
+    if messagebox.askokcancel("Выход из игры", "Ты чё уже уходишь?"):
+        app_running = False
+        tk.destroy()
+
+tk.protocol("WM_DELETE_WINDOW", on_closing)
+tk.title("Игра Морско  бой")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=size_canvas_x, height=size_canvas_y, bd=0, highlightthickness=0)
+canvas.create_rectangle(0, 0, size_canvas_y, size_canvas_y, fill="#fafaf1")
+canvas.pack()
+tk.update()
+
+while app_running:
+    if app_running:
+        tk.update_idletasks()
+        tk.update()
+    time.sleep(0.005)
