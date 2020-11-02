da45b383d3941e4c87b3e53384c9842cda1c8489
Нуфтанчики <300225@mail.ru>
2020-11-02 23:03:19 +0300

    добавил клетки на поле

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ app_running = True
 
 size_canvas_x = 800
 size_canvas_y = 800
+kletok_po_x = kletok_po_y = 7
+step_x = size_canvas_x // kletok_po_x
+step_y = size_canvas_y // kletok_po_y
+size_canvas_x = step_x * kletok_po_x
+size_canvas_y = step_y * kletok_po_y
+
 
 
 def on_closing():
@@ -24,6 +30,16 @@ canvas = Canvas(tk, width=size_canvas_x, height=size_canvas_y, bd=0, highlightth
 canvas.create_rectangle(0, 0, size_canvas_y, size_canvas_y, fill="#fafaf1")
 canvas.pack()
 tk.update()
+
+
+def draw_table():
+    for i in range(0, kletok_po_x + 1):
+        canvas.create_line(step_x * i, 0, step_y * i, size_canvas_y)
+
+    for i in range(0, kletok_po_x + 1):
+        canvas.create_line(0, step_y * i, size_canvas_y, step_x * i)
+
+draw_table()
 
 while app_running:
     if app_running:
